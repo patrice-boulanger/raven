a8e744525128a2c7b363bbb59e7d70a86e41e3ee
Patrice Boulanger <patrice.boulanger92@gmail.com>
2017-02-08 21:44:34 +0100

    change update() to read all axis at the same time

--- hmc5883l.cpp
+++ hmc5883l.cpp
@@ -8,53 +8,48 @@
 
 #include "hmc5883l.h"
 
-int X_value, Y_value, Z_value;
-double module, angle;
+int X_axis, Y_axis, Z_axis;
+double angle;
 
 void HMC5883L_init(uint8_t regA, uint8_t regB)
 {
 	Wire.beginTransmission(HMC5883L_ADDR);
 	Wire.write(0x00);
-	Wire.write(regA); // 0x70
+	Wire.write(regA);
 
 	Wire.write(0x01);
-	Wire.write(regB); // 0xA0   
-	Wire.endTransmission();
-}
+	Wire.write(regB);   
 
-int HMC5883L_read_axis(uint8_t axis)
-{
-	int res;
-	
-	Wire.beginTransmission(HMC5883L_ADDR);
-	Wire.write(0x02);
-	Wire.write(0x01);
-	Wire.endTransmission();
-	
-	delay(6);
-
-	Wire.beginTransmission(HMC5883L_ADDR);
-	Wire.write(axis);
-	Wire.endTransmission();
-
-	Wire.requestFrom(HMC5883L_ADDR, 2);
-	res = Wire.read() << 8;
-	res |= Wire.read();
-
-	return res;
+  Wire.write(0x02); //select mode register
+  Wire.write(0x00); //continuous measurement mode
+  Wire.endTransmission();
 }
 
 void HMC5883L_update(void)
 {
-	X_value = HMC5883L_read_axis(HMC5883L_X);  
-	Y_value = HMC5883L_read_axis(HMC5883L_Y);
-	Z_value = HMC5883L_read_axis(HMC5883L_Z);
-
-	module = X_value * X_value + Y_value * Y_value + Z_value * Z_value;
-	module = sqrt(module);
+  Wire.beginTransmission(HMC5883L_ADDR);
+  Wire.write(0x03); //select register 3, X MSB register
+  Wire.endTransmission();
+  
+  //Read data from each axis, 2 registers per axis
+  Wire.requestFrom(HMC5883L_ADDR, 6);
+  
+  while(Wire.available() < 6);
+  
+  X_axis = Wire.read() << 8; // X msb
+  X_axis |= Wire.read();     // X lsb
+  Z_axis = Wire.read() << 8; // Z msb
+  Z_axis |= Wire.read();     // Z lsb
+  Y_axis = Wire.read() << 8; // Y msb
+  Y_axis |= Wire.read();     // Y lsb
 
 	// Compas angle in XY-plan
-	angle = atan2(Y_value, X_value) * (180 / 3.14159265);
+	angle = atan2(Y_axis, X_axis) * (180 / 3.14159265);
 	if (angle < 0) 	       
 		angle = angle + 360; 
+/*
+  Serial.print("HMC5883L: compas angle = ");
+  Serial.print(angle);
+  Serial.println(" deg");
+  */
 }
