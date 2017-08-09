cf4d0b857cea309b310714c0b1d21d1cdae6469e
Patrice Boulanger <patrice.boulanger92@gmail.com>
2017-08-09 22:59:33 +0200

    debugging

--- raven/buzzer.cpp
+++ raven/buzzer.cpp
@@ -2,14 +2,14 @@
 #include "buzzer.h"
 
 /*
- * A melody is a sequence of frequency/duration, ended by 0/0:
+ * A melody is a sequence of frequency/duration, ended by 0/0/0:
  *
- * { freq, duration, freq, duration, ..., 0, 0 }
+ * { freq, duration, pause, freq, duration, pause, ..., 0, 0, 0 }
  */
 // BUZZER_START
-int buz_start[] = { 440, 500, 440, 250, 440, 250, 0, 0 };
+int buz_start[] = { 2200, 100, 150, 2200, 100, 150, 2200, 100, 150, 0, 0, 0 };
 // BUZZER_READY
-int buz_ready[] = { 220, 500, 440, 500, 880, 500, 0, 0 };
+int buz_ready[] = { 2200, 80, 100, 2200, 80, 100, 2200, 80, 100, 2200, 80, 100, 2200, 80, 100, 0, 0, 0 };
  
 int * melodies[] = {
         buz_start, 
@@ -21,9 +21,12 @@ void buzzer_play(uint8_t melody)
 	if (melody >= sizeof(melodies) / sizeof(melodies[0]))
 		return;
 
-	for(int *freq = melodies[melody], *duration = (freq+1) ; *freq != 0; freq ++, duration ++) {
+	for(int *freq = melodies[melody], *duration = (freq+1), *pause = (freq+2); 
+		*freq != 0 && *duration != 0 && *pause != 0; 
+			freq += 2, duration += 2, pause += 2) {
+				
 		tone(BUZZER_PIN, *freq, *duration);
-		delay(*duration + 10);
+		delay(*pause);
 	}
 
         noTone(BUZZER_PIN);
